a8e3756191298a7ab160c3c385950077f04886b1
Neal Wang <nealwang.sh@protonmail.com>
2024-10-23 10:20:57 -0700

    feat: assignment 7 requirement 4

---
+++ 214/assignments/assignment7/requirement4/assignment7.srcs/sources_1/new/main.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module register(
+	input [7:0] D,
+	input En,
+	input Clk,
+	output [7:0] Q
+);
+	reg [7:0] tmp;
+
+	always @ (posedge Clk) begin
+		if (En) begin
+			tmp <= D;
+		end
+	end
+
+	assign Q = tmp;
+endmodule
+
+module mux(
+	input [7:0] I0,
+	input [7:0] I1,
+	input sel,
+	output [7:0] O
+);
+	reg [7:0] res;
+
+	always @ (I0, I1, sel) begin
+		case (sel)
+			0: res <= I0;
+			1: res <= I1;
+		endcase
+	end
+
+	assign O = res;
+endmodule
+
+module main(
+	input [7:0] sw,
+	input [1:0] btn,
+	input clk,
+	output [7:0] led
+);
+	wire [7:0] reg_to_mux;
+
+	register r(.D(sw), .En(btn[0]), .Clk(clk), .Q(reg_to_mux));
+	mux m(.I0(sw), .I1(reg_to_mux), .sel(btn[1]), .O(led));
+endmodule
