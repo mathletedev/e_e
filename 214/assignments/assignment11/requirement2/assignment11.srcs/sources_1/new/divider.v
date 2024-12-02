e73d28690562d887ac57809259fb89fbc3182735
Neal Wang <nealwang.sh@protonmail.com>
2024-12-02 09:43:44 -0800

    fix: don't reset clock divider to allow for reset

--- 214/assignments/assignment11/requirement2/assignment11.srcs/sources_1/new/divider.v
+++ 214/assignments/assignment11/requirement2/assignment11.srcs/sources_1/new/divider.v
@@ -8,17 +8,17 @@ module divider (
     reg [17:0] counter;
 
     always @(posedge clk) begin
-        if (rst) begin
-            res <= 0;
+        // BUG: can't reset the clock divider since it clocks the counter
+        // if (rst) begin
+        //     res <= 0;
+        //     counter <= 0;
+        // end
+        if (counter == 100000) begin
+            res <= 1;
             counter <= 0;
         end else begin
-            if (counter == 100000) begin
-                res <= 1;
-                counter <= 0;
-            end else begin
-                res <= 0;
-                counter <= counter + 1;
-            end
+            res <= 0;
+            counter <= counter + 1;
         end
     end
 endmodule
